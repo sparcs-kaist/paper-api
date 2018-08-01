73858a4a9cdde00226e34778bce8061cd97fed54
sparcsJara <jara0701@gmail.com>
2018-08-01 20:27:29 +0900

    api to get participates which user created

--- paper/api/answers/views.py
+++ paper/api/answers/views.py
@@ -41,6 +41,19 @@ class ParticipateViewSet(viewsets.ModelViewSet, ActionAPIViewSet):
             author=self.request.user
         )
 
+    @action(methods=['get'], detail=False)
+    def created(self, request):
+        if request.user.is_anonymous:
+            return Response({'Message': 'You are unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = request.user
+        queryset = Participate.objects.filter(author=user).order_by('updated_time')
+        page = self.paginate_queryset(queryset)
+        serializer = ParticipateListSerializer(page, many=True, context={
+            'request': request,
+        })
+        if page is not None:
+            return self.get_paginated_response(serializer.data)
+        return Response(serializer.data)
 
 class AnswerViewSet(viewsets.ModelViewSet):
     """
